86b9dc9c1217b8aed804f903b115a368dce3a67c
kakeru2025 <2019.saki.keru.2023@gmail.com>
2023-07-23 15:00:26 +0900

    seeder仮置き1_0723

---
+++ database/seeders/YearSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use DateTime;
+
+class YearSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('years')->insert([
+            'year' => '2022',
+        ]);
+        
+         DB::table('years')->insert([
+            'year' => '2021',
+        ]);
+        
+         DB::table('years')->insert([
+            'year' => '2020',
+        ]);
+    }
+}
